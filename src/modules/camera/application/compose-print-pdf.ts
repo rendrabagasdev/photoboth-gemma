401f12341622f1d347864b89776112f73e9dc28c
rendrabagasdev <rendrabagasdev@gmail.com>
2026-08-25 00:19:00 +0700

    add

--- src/modules/camera/application/compose-print-pdf.ts
+++ src/modules/camera/application/compose-print-pdf.ts
@@ -2,6 +2,7 @@ import { PRINT_HEIGHT, PRINT_WIDTH } from '../domain/template-layout'
 
 const PAGE_WIDTH_PT = (10.6 / 2.54) * 72
 const PAGE_HEIGHT_PT = (15.8 / 2.54) * 72
+
 function encode(value: string): Uint8Array<ArrayBuffer> {
   return new TextEncoder().encode(value)
 }
